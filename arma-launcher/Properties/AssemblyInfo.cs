6206e508a3a97a1607d70ca5e06e260c60b3f797
Tim Sazon <tim.sazon51@gmail.com>
2019-06-22 01:31:18 +0400

    0.2.2

--- arma-launcher/Properties/AssemblyInfo.cs
+++ arma-launcher/Properties/AssemblyInfo.cs
@@ -18,5 +18,5 @@ using System.Windows;
     ResourceDictionaryLocation.SourceAssembly
 )]
 
-[assembly: AssemblyVersion("0.2.1")]
-[assembly: AssemblyFileVersion("0.2.1")]
+[assembly: AssemblyVersion("0.2.2")]
+[assembly: AssemblyFileVersion("0.2.2")]
